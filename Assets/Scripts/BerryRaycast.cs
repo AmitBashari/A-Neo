c2832db19726791776c2b7d6c25a554cb1f34858
AmitBashari <bashari.amit@gmail.com>
2020-08-10 15:51:07 +0300

    Trying to add a SuperBerry

---
+++ Assets/Scripts/BerryRaycast.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BerryRaycast : MonoBehaviour
+{
+    //public GameObject Player;
+    public BoxCollider2D PlayerBoxCollider2D;
+    public FollowWP RaycastFollowWP;
+
+
+    private float _distance = 1f;
+
+    private void Awake()
+    {
+        PlayerBoxCollider2D = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>();
+        RaycastFollowWP = GetComponent<FollowWP>();
+
+
+
+    }
+    void Update()
+    {
+        /*
+        Vector2 start = transform.position;
+        Vector2 direction = (Player.transform.position - transform.position).normalized;
+
+        
+        Debug.DrawRay(start, direction * _distance, Color.red);
+
+        
+        RaycastHit2D[] sightTestResults = Physics2D.RaycastAll(start, direction, _distance);
+
+        //now iterate over all results to work out what has happened
+        for (int i = 0; i < sightTestResults.Length; i++)
+        {
+            RaycastHit2D sightTest = sightTestResults[i];
+
+            gameObject.GetComponent<FollowWP>().enabled = false;
+        }
+        */
+
+        Debug.DrawRay(transform.position, transform.InverseTransformDirection(Vector2.up) * _distance, Color.red);
+        Debug.DrawRay(transform.position, transform.InverseTransformDirection(Vector2.down) * _distance, Color.red);
+        Debug.DrawRay(transform.position, transform.InverseTransformDirection(Vector2.left) * _distance, Color.red);
+        Debug.DrawRay(transform.position, transform.InverseTransformDirection(Vector2.right) * _distance, Color.red);
+
+        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, transform.InverseTransformDirection(Vector2.up), _distance);
+        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, transform.InverseTransformDirection(Vector2.down), _distance);
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, transform.InverseTransformDirection(Vector2.left), _distance);
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, transform.InverseTransformDirection(Vector2.right), _distance);
+
+
+        if (hitLeft.collider == PlayerBoxCollider2D)
+        {
+            Debug.Log("I hit Player");
+
+            RaycastFollowWP.ReverseArray();
+            //_distance = 0;
+            
+        }
+    }
+
+}
---
+++ Assets/Scripts/FollowWP.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWP : MonoBehaviour
+{
+    public Transform[] Waypoints;
+
+    [SerializeField]
+    float Movespeed = 10f;
+
+    public int WaypointsNumber = 0;
+
+    void Start()
+    {
+        transform.position = Waypoints[WaypointsNumber].transform.position;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+        Waypoints[WaypointsNumber].transform.position,
+        Movespeed * Time.deltaTime);
+
+        if (transform.position == Waypoints[WaypointsNumber].transform.position)
+        {
+            WaypointsNumber += 1;
+        }
+
+        if (WaypointsNumber == Waypoints.Length)
+            WaypointsNumber = 0;
+    }
+
+    public void ReverseArray()
+    {
+        //WaypointsNumber--;
+        //System.Array.Reverse(Waypoints);
+
+
+    }
+}
